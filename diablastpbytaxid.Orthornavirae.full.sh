61c92b2446f71161afe20f7747be31fc69cd0c51
lynceuslq <55744039+lynceuslq@users.noreply.github.com>
2021-07-09 03:13:17 +0100

    Update diablastpbytaxid.Orthornavirae.full.sh

--- diablastpbytaxid.Orthornavirae.full.sh
+++ diablastpbytaxid.Orthornavirae.full.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-DIAMOND="/hwfssz5/ST_INFECTION/GlobalDatabase/user/fengqikai/software/Diamond/WHICHDIAMOD"
-DIAMOND_DB="/jdfssz1/ST_HEALTH/P20Z10200N0206/fengqikai/NCBI-BLAST/2021-05/NR/Diamond/WHICHNRDMND"
-INDIR="/hwfssz5/ST_INFECTION/GlobalDatabase/user/liqian6/new_taxdump"
-OUTDIR="/hwfssz5/ST_INFECTION/GlobalDatabase/user/liqian6/WHICHOUTPATH"
-familylist="/hwfssz5/ST_INFECTION/GlobalDatabase/user/liqian6/new_taxdump/WHICHFAMLIST"
-Infile="fromSM.rdrp20201015.fasta"
-OUTFAMDIR="WHICHDIRTOSTOREFAMS"
+DIAMOND="/PATH/TO/DIAMOD"
+BLAST_BIN="/PATH/TO/BLAST/BIN"
+CDD_BLASTPROFILE=""
+DIAMOND_DB="/PATH/TO/NR/Diamond/nr.dmnd"
+INDIR="/PATH/TO/INPUT"
+OUTDIR="PATH/TO/TEMPERATE/OUTPUT"
+familylist="FILE.OF.A.LIST.OF.FAMILIES.TXT"
+Infile="SEED.PROTEIN.SEQUENCES.FASTA"
+OUTFAMDIR="PATH/TO/STORE/RESULTS"
+FAMILY_SCRIPTS="PATH/TO/rmrdseqbyfam.sh"
+QSUB="FULL_QSUB_ARGUEMENTS" #if you do not have a computer cluster to work on, just type in use "bash" or "nohup" here, should work the same
 
 echo -e "Starting working on $Infile at $(date)"
 
@@ -14,17 +18,17 @@ $DIAMOND  blastp --db  $DIAMOND_DB -q $INDIR/$Infile -f 6 qseqid sseqid qlen sle
 
 echo -e "blastp completed at $(date)"
 
-cat $OUTDIR/$Infile.blastpout | tr " " "+" | cut -f2,12,11,14 | sort -u -T /ldfssz1/ST_INFECTION/P20Z10200N0206_pathogendb/liqian6 |while read f1 f2 f3 f4; do echo -e ">${f1// /}\t|\t${f2// /}\t|\t${f3// /}\n${f4// /}"; done | tr "+" " " > $OUTDIR/$Infile.sigblastp.faa
+cat $OUTDIR/$Infile.blastpout | tr " " "+" | cut -f2,12,11,14 | sort -u -T $OUTDIR |while read f1 f2 f3 f4; do echo -e ">${f1// /}\t|\t${f2// /}\t|\t${f3// /}\n${f4// /}"; done | tr "+" " " > $OUTDIR/$Infile.sigblastp.faa
 
 echo -e "extraction from blastp results completed at $(date)"
 
 echo -e "Starting to compare CDDs against selected sequences with psi-blast at $(date)"
 
-/hwfssz1/ST_META/AP/zhaohailong/ncbi-blast-2.10.1+/bin/psiblast -query  $OUTDIR/$Infile.sigblastp.faa -outfmt 6 -evalue 1E-5 -out $OUTDIR/$Infile.cdds.psiblast -db /hwfssz5/ST_INFECTION/GlobalDatabase/share/public_database/rdrp_AAseq/CDD/cdd.smp.pn/all.html.accession  -num_threads 16
+$BLAST_BIN/psiblast -query  $OUTDIR/$Infile.sigblastp.faa -outfmt 6 -evalue 1E-5 -out $OUTDIR/$Infile.cdds.psiblast -db $CDD_BLASTPROFILE -num_threads 16
 
 echo -e "Starting to compare CDDs against selected sequences with rps-blast at $(date)"
-$
-/hwfssz1/ST_META/AP/zhaohailong/ncbi-blast-2.10.1+/bin/rpsblast -query  $OUTDIR/$Infile.sigblastp.faa -outfmt 6 -evalue 1E-5 -out $OUTDIR/$Infile.cdds.rpsblast -db /hwfssz5/ST_INFECTION/GlobalDatabase/share/public_database/rdrp_AAseq/CDD/cdd.smp.pn/all.html.accession  -num_threads 16
+
+$BLAST_BIN/rpsblast -query  $OUTDIR/$Infile.sigblastp.faa -outfmt 6 -evalue 1E-5 -out $OUTDIR/$Infile.cdds.rpsblast -db $CDD_BLASTPROFILE -num_threads 16
 
 cat $OUTDIR/$Infile.cdds.psiblast $OUTDIR/$Infile.cdds.rpsblast | cut -f1 | sort | uniq > $OUTDIR/$Infile.selected.list
 
@@ -40,5 +44,6 @@ cat $OUTDIR/$Infile.allsig.list | grep ";" | head | while read f1 f2 f3 f4 f5; d
 
 echo -e "finishing at $(date)"
 
-cat $familylist | while read fam; do cp /hwfssz5/ST_INFECTION/GlobalDatabase/user/liqian6/new_taxdump/rmrdseqbyfam.sh $OUTDIR/rmrdseqbyfam.${fam// /}.tmp; sed -i "s/TESTFAM/$(echo ${fam}  | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp ; sed -i "s/TESTWORKDIR/$(echo ${OUTFAMDIR} | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp; sed -i "s/TESTINPATH/$(echo ${OUTDIR}  | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp; sed -i "s/INPUTFASTA/$(echo ${Infile} | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp ; cat $OUTDIR/rmrdseqbyfam.${fam// /}.tmp | tr "@" "/" > $OUTDIR/rmrdseqbyfam.${fam// /}.sh; rm $OUTDIR/rmrdseqbyfam.${fam// /}.tmp ; echo -e "script for $fam is prepared, please run it in $OUTDIR" ; done
+cat $familylist | while read fam; do cp $FAMILY_SCRIPTS $OUTDIR/rmrdseqbyfam.${fam// /}.tmp; sed -i "s/TESTFAM/$(echo ${fam}  | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp ; sed -i "s/TESTWORKDIR/$(echo ${OUTFAMDIR} | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp; sed -i "s/TESTINPATH/$(echo ${OUTDIR}  | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp; sed -i "s/INPUTFASTA/$(echo ${Infile} | tr "/" "@")/" $OUTDIR/rmrdseqbyfam.${fam// /}.tmp ; cat $OUTDIR/rmrdseqbyfam.${fam// /}.tmp | tr "@" "/" > $OUTDIR/rmrdseqbyfam.${fam// /}.sh; rm $OUTDIR/rmrdseqbyfam.${fam// /}.tmp ; echo -e "script for $fam is prepared, please run it in $OUTDIR" ; done
  
+cat $familylist | while read fam; do $QSUB $OUTDIR/rmrdseqbyfam.${fam// /}.sh ; echo -e "script for viral family $fam has been submitted at $(date)" ; done
